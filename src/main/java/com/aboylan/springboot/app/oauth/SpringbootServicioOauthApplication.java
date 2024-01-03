e118665a71b147fcddfd040fe6b8767b751d9003
Alex Boylan <aboylan@users.noreply.github.com>
2024-01-03 10:41:19 -0600

    Creando nuestra interfaz Feign Client

--- src/main/java/com/aboylan/springboot/app/oauth/SpringbootServicioOauthApplication.java
+++ src/main/java/com/aboylan/springboot/app/oauth/SpringbootServicioOauthApplication.java
@@ -2,7 +2,9 @@ package com.aboylan.springboot.app.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioOauthApplication {
 
