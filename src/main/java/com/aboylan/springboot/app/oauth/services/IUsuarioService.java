7ae95782f9fa7dba62cf7bc827cbfcb38e212df0
Alex Boylan <aboylan@users.noreply.github.com>
2024-01-04 22:41:38 -0600

    Anadiendo mas informacion al token JWT

---
+++ src/main/java/com/aboylan/springboot/app/oauth/services/IUsuarioService.java
@@ -0,0 +1,9 @@
+package com.aboylan.springboot.app.oauth.services;
+
+import com.aboylan.springboot.app.commons.usuarios.models.entity.Usuario;
+
+public interface IUsuarioService {
+
+	public Usuario findByUsername(String username);
+
+}
--- src/main/java/com/aboylan/springboot/app/oauth/services/UsuarioService.java
+++ src/main/java/com/aboylan/springboot/app/oauth/services/UsuarioService.java
@@ -18,7 +18,7 @@ import com.aboylan.springboot.app.commons.usuarios.models.entity.Usuario;
 import com.aboylan.springboot.app.oauth.clients.UsuarioFeignClient;
 
 @Service
-public class UsuarioService implements UserDetailsService {
+public class UsuarioService implements UserDetailsService, IUsuarioService {
 
 	private Logger log = LoggerFactory.getLogger(UsuarioService.class);
 
@@ -41,7 +41,13 @@ public class UsuarioService implements UserDetailsService {
 
 		log.info("Usuario autenticado: " + username);
 
-		return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true, authorities);
+		return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true,
+				authorities);
+	}
+
+	@Override
+	public Usuario findByUsername(String username) {
+		return client.findByUsername(username);
 	}
 
 }
