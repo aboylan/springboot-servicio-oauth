fffeb2822bc9f72ee07372bbece87e5b0061919e
Alex Boylan <aboylan@users.noreply.github.com>
2024-01-10 17:26:51 -0600

    Implementando evento tres intentos en el login - parte 1

--- src/main/java/com/aboylan/springboot/app/oauth/security/event/AuthenticationSuccessErrorHandler.java
+++ src/main/java/com/aboylan/springboot/app/oauth/security/event/AuthenticationSuccessErrorHandler.java
@@ -2,6 +2,7 @@ package com.aboylan.springboot.app.oauth.security.event;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -9,19 +10,24 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
+import com.aboylan.springboot.app.oauth.services.IUsuarioService;
+
 @Component
 public class AuthenticationSuccessErrorHandler implements AuthenticationEventPublisher {
 
 	private Logger log = LoggerFactory.getLogger(AuthenticationSuccessErrorHandler.class);
 
+	@Autowired
+	private IUsuarioService usuarioService;
+
 	@Override
 	public void publishAuthenticationSuccess(Authentication authentication) {
-		
+
 		// if (authentication.getName().equalsIgnoreCase("frontendapp")) {
 		if (authentication.getDetails() instanceof WebAuthenticationDetails) {
 			return;
 		}
-		
+
 		UserDetails user = (UserDetails) authentication.getPrincipal();
 		String mensaje = "Success Login: " + user.getUsername();
 		System.out.println(mensaje);
