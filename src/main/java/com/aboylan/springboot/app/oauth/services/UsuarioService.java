8df0c8d8202321564c44e1931e06e02f5b1740b2
Alex Boylan <aboylan@users.noreply.github.com>
2024-01-10 18:32:13 -0600

    Manejando error 404 en componente de servicio UserDetailsService

--- src/main/java/com/aboylan/springboot/app/oauth/services/UsuarioService.java
+++ src/main/java/com/aboylan/springboot/app/oauth/services/UsuarioService.java
@@ -17,6 +17,8 @@ import org.springframework.stereotype.Service;
 import com.aboylan.springboot.app.commons.usuarios.models.entity.Usuario;
 import com.aboylan.springboot.app.oauth.clients.UsuarioFeignClient;
 
+import feign.FeignException;
+
 @Service
 public class UsuarioService implements UserDetailsService, IUsuarioService {
 
@@ -27,22 +29,24 @@ public class UsuarioService implements UserDetailsService, IUsuarioService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Usuario usuario = client.findByUsername(username);
 
-		if (usuario == null) {
+		try {
+			Usuario usuario = client.findByUsername(username);
+
+			List<GrantedAuthority> authorities = usuario.getRoles().stream()
+					.map(role -> new SimpleGrantedAuthority(role.getNombre()))
+					.peek(authority -> log.info("Role: " + authority.getAuthority())).collect(Collectors.toList());
+
+			log.info("Usuario autenticado: " + username);
+
+			return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true,
+					authorities);
+
+		} catch (FeignException e) {
 			log.error("Error en el login, no existe el usuario '" + username + "' en el sistema");
 			throw new UsernameNotFoundException(
 					"Error en el login, no existe el usuario '" + username + "' en el sistema");
 		}
-
-		List<GrantedAuthority> authorities = usuario.getRoles().stream()
-				.map(role -> new SimpleGrantedAuthority(role.getNombre()))
-				.peek(authority -> log.info("Role: " + authority.getAuthority())).collect(Collectors.toList());
-
-		log.info("Usuario autenticado: " + username);
-
-		return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true,
-				authorities);
 	}
 
 	@Override
