bc7a817e9789569dec4d712c144c5582fd67f5a2
Alex Boylan <aboylan@users.noreply.github.com>
2024-01-10 18:24:03 -0600

    Implementando evento tres intentos en el login - parte 2

--- src/main/java/com/aboylan/springboot/app/oauth/security/event/AuthenticationSuccessErrorHandler.java
+++ src/main/java/com/aboylan/springboot/app/oauth/security/event/AuthenticationSuccessErrorHandler.java
@@ -10,7 +10,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
+import com.aboylan.springboot.app.commons.usuarios.models.entity.Usuario;
 import com.aboylan.springboot.app.oauth.services.IUsuarioService;
+
+import feign.FeignException;
 
 @Component
 public class AuthenticationSuccessErrorHandler implements AuthenticationEventPublisher {
@@ -32,6 +35,13 @@ public class AuthenticationSuccessErrorHandler implements AuthenticationEventPub
 		String mensaje = "Success Login: " + user.getUsername();
 		System.out.println(mensaje);
 		log.info(mensaje);
+
+		Usuario usuario = usuarioService.findByUsername(authentication.getName());
+
+		if (usuario.getIntentos() != null && usuario.getIntentos() > 0) {
+			usuario.setIntentos(0);
+			usuarioService.update(usuario, usuario.getId());
+		}
 	}
 
 	@Override
@@ -39,6 +49,29 @@ public class AuthenticationSuccessErrorHandler implements AuthenticationEventPub
 		String mensaje = "Error en el Login: " + exception.getMessage();
 		log.error(mensaje);
 		System.out.println(mensaje);
+
+		try {
+			Usuario usuario = usuarioService.findByUsername(authentication.getName());
+			if (usuario.getIntentos() == null) {
+				usuario.setIntentos(0);
+			}
+
+			log.info("Intento actual es de: " + usuario.getIntentos());
+
+			usuario.setIntentos(usuario.getIntentos() + 1);
+
+			log.info("Intento despues es de: " + usuario.getIntentos());
+
+			if (usuario.getIntentos() >= 3) {
+				log.error(String.format("El usuario %s deshabilitado por maximos intentos", usuario.getUsername()));
+				usuario.setEnabled(false);
+			}
+
+			usuarioService.update(usuario, usuario.getId());
+
+		} catch (FeignException e) {
+			log.error(String.format("El usuario %s no existe en el sistema", authentication.getName()));
+		}
 	}
 
 }
